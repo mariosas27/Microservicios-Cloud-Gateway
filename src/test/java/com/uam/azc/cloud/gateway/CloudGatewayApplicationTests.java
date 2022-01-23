ebe12cc84fb31137db5f14fc04164d4ef55635f2
Juan Mario Sastre Cuahutle <mariosas27@gmail.com>
2022-01-23 13:41:19 -0600

    Pimmer commit, se agregaron todos los archivos

---
+++ src/test/java/com/uam/azc/cloud/gateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uam.azc.cloud.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
